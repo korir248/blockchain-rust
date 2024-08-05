dc3c7f862d27a87608892ed60e12a1d6c0d55dfe
Eugene Korir <korir16071@gmail.com>
2024-08-06 02:09:16 +0300

    Added impl for Blockchain struct adding methods such as mine_block, find_transactions etc

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,173 @@
-use crate::block::Block;
+use std::{
+    collections::HashMap,
+    env::current_dir,
+    sync::{Arc, RwLock},
+};
+
+use data_encoding::HEXLOWER;
+use sled::{transaction::TransactionResult, Db, Tree};
+
+use crate::{
+    block::Block,
+    transaction::{TXOutput, Transaction},
+};
+
+const TIP_BLOCK_HASH_KEY: &str = "tip_block_hash";
+const BLOCKS_TREE: &str = "blocks";
 
 pub struct Blockchain {
-    nlocks: Vec<Block>,
+    pub tip_hash: Arc<RwLock<String>>,
+    pub db: Db,
+}
+
+impl Blockchain {
+    pub fn new_blockchain() -> Blockchain {
+        let db = sled::open(current_dir().unwrap().join("data")).unwrap();
+
+        let blocks_tree = db.open_tree(BLOCKS_TREE).unwrap();
+        let tip_bytes = blocks_tree
+            .get(TIP_BLOCK_HASH_KEY)
+            .unwrap()
+            .expect("No existing blockchain found. Create one first.");
+        let tip_hash = String::from_utf8(tip_bytes.to_vec()).unwrap();
+        Blockchain {
+            tip_hash: Arc::new(RwLock::new(tip_hash)),
+            db,
+        }
+    }
+    pub fn create_blockchain(genesis_address: &str) -> Blockchain {
+        let db = sled::open(current_dir().unwrap().join("data")).unwrap();
+        let blocks_tree = db.open_tree(BLOCKS_TREE).unwrap();
+        let data = blocks_tree.get(TIP_BLOCK_HASH_KEY).unwrap();
+        let tip_hash;
+        match data {
+            None => {
+                let coinbase_tx = Transaction::new_coinbase_tx(genesis_address);
+                let block = Block::generate_genesis_block(&coinbase_tx);
+                Self::update_blocks_tree(&blocks_tree, &block);
+                tip_hash = String::from(block.get_hash());
+            }
+            Some(data) => {
+                tip_hash = String::from_utf8(data.to_vec()).unwrap();
+            }
+        }
+        Blockchain {
+            tip_hash: Arc::new(RwLock::new(tip_hash)),
+            db,
+        }
+    }
+    pub fn mine_block(&self, transactions: &[Transaction]) -> Block {
+        for transaction in transactions {
+            if transaction.verify(self) == false {
+                panic!("ERROR: Invalid transaction")
+            }
+        }
+        let best_height = self.get_best_height();
+        let block = Block::new_block(self.get_tip_hash(), transactions, best_height + 1);
+        let block_hash = block.get_hash();
+        let blocks_tree = self.db.open_tree(BLOCKS_TREE).unwrap();
+        Self::update_blocks_tree(&blocks_tree, &block);
+        self.set_tip_hash(block_hash);
+        block
+    }
+
+    fn update_blocks_tree(blocks_tree: &Tree, block: &Block) {
+        let block_hash = block.get_hash();
+        let _: TransactionResult<(), ()> = blocks_tree.transaction(|tx_db| {
+            let _ = tx_db.insert(block_hash, block.clone());
+            let _ = tx_db.insert(TIP_BLOCK_HASH_KEY, block_hash);
+            Ok(())
+        });
+    }
+    pub fn add_block(&self, block: &Block) {
+        let block_tree = self.db.open_tree(BLOCKS_TREE).unwrap();
+        if let Some(_) = block_tree.get(block.get_hash()).unwrap() {
+            return;
+        }
+        let _: TransactionResult<(), ()> = block_tree.transaction(|tx_db| {
+            let _ = tx_db.insert(block.get_hash(), block.serialize()).unwrap();
+            let tip_block_bytes = tx_db
+                .get(self.get_tip_hash())
+                .unwrap()
+                .expect("The tip hash is not valid");
+            let tip_block = Block::deserialize(tip_block_bytes.as_ref());
+            if block.get_height() > tip_block.get_height() {
+                let _ = tx_db.insert(TIP_BLOCK_HASH_KEY, block.get_hash()).unwrap();
+                self.set_tip_hash(block.get_hash());
+            }
+            Ok(())
+        });
+    }
+
+    pub fn find_transaction(&self, txid: &[u8]) -> Option<Transaction> {
+        let mut iterator = self.iterator();
+        loop {
+            let option = iterator.next();
+            if option.is_none() {
+                break;
+            }
+            let block = option.unwrap();
+            for transaction in block.get_transactions() {
+                if txid.eq(transaction.get_id()) {
+                    return Some(transaction.clone());
+                }
+            }
+        }
+        None
+    }
+    pub fn get_db(&self) -> &Db {
+        &self.db
+    }
+    pub fn get_tip_hash(&self) -> String {
+        self.tip_hash.read().unwrap().clone()
+    }
+    pub fn set_tip_hash(&self, new_tip_hash: &str) {
+        let mut tip_hash = self.tip_hash.write().unwrap();
+
+        *tip_hash = String::from(new_tip_hash);
+    }
+
+    pub fn iterator(&self) -> BlockchainIterator {
+        BlockchainIterator::new(self.get_tip_hash(), self.db.clone())
+    }
+
+    pub fn get_best_height(&self) -> usize {
+        todo!()
+    }
+
+    pub fn find_utxo(&self) -> HashMap<String, Vec<TXOutput>> {
+        let mut utxo: HashMap<String, Vec<TXOutput>> = HashMap::new();
+        let mut spent_txos: HashMap<String, Vec<usize>> = HashMap::new();
+        let mut iterator = self.iterator();
+        loop {
+            let option = iterator.next();
+            if option.is_none() {
+                break;
+            }
+            let block = option.unwrap();
+            'outer: for tx in block.get_transactions() {
+                let txid_hex = HEXLOWER.encode(tx.get_id());
+                for (idx, out) in tx.get_vout().iter().enumerate() {}
+            }
+        }
+        utxo
+    }
+}
+
+pub struct BlockchainIterator {
+    db: Db,
+    current_hash: String,
+}
+
+impl BlockchainIterator {
+    pub fn new(tip_hash: String, db: Db) -> Self {
+        Self {
+            db,
+            current_hash: tip_hash,
+        }
+    }
+
+    pub fn next(&mut self) -> Option<Block> {
+        todo!()
+    }
 }
